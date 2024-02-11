d2b4328be8b4c6c592613db6a275ba8b57daf996
Tantos Alexandra <alexandra.tantos@yahoo.com>
2024-02-11 12:40:26 +0200

    initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Wrapper from "../assets/wrappers/AboutPage";
+const About = () => {
+  return (
+    <Wrapper>
+      <h3>About Us</h3>
+      <p>
+        Introducing "MixMaster" — the perfect party companion app! It
+        effortlessly fetches entertaining cocktail recipes from the lively
+        Cocktails DB API. With a simple swipe, you'll discover a variety of
+        delightful drink ideas that are bound to excite your taste buds and
+        bring joy to your friends. Elevate your mixology game, explore fantastic
+        mocktail options, and let the laughter flow!
+      </p>
+    </Wrapper>
+  );
+};
+
+export default About;
